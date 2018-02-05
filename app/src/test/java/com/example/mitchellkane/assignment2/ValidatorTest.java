879b9a7228d15737010711e6dc9312123eded0b5
MitchellLeeKane <mkane@dal.ca>
2018-02-05 18:35:52 -0400

    Updated test cases for A2-stage1

--- app/src/test/java/com/example/mitchellkane/assignment2/ValidatorTest.java
+++ app/src/test/java/com/example/mitchellkane/assignment2/ValidatorTest.java
@@ -8,28 +8,31 @@ import static org.junit.Assert.*;
 
 /**
  * Created by mitchellkane on 2018-02-05.
+ * tests if a password matches a number of conditions
  */
 public class ValidatorTest {
     Validator v = new Validator();
 
     @Before
 
-
-    @Test // test for password being "password"
+    @Test // test for password not being "password"
     public void matchesPassword() throws Exception {
+        assertEquals(true, v.isPassword(""));
+        assertEquals(true, v.isPassword("a"));
         assertEquals(false, v.isPassword("password"));
-        assertEquals(false, v.isPassword("Password"));
-        assertEquals(true, v.isPassword("Password1"));
+        assertEquals(false, v.isPassword("PasSWord"));
+        assertEquals(true, v.isPassword("Pass word"));
+        assertEquals(true, v.isPassword("password1"));
     }
 
     @Test // test for the length of password
     public void checkLength() throws Exception {
-        assertEquals(false, v.greater7("frog"));
+        assertEquals(false, v.greater7(""));
+        assertEquals(false, v.greater7("a"));
+        assertEquals(false, v.greater7("isSeven"));
+        assertEquals(true, v.greater7("password"));
+        assertEquals(true, v.greater7("itsEight"));
         assertEquals(true, v.greater7("pineapple"));
     }
-
-
-
-
 
 }
