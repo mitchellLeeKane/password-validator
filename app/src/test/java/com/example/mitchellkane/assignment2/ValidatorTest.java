81e0243d26135f6108a0cef6ff897de4ecb984ee
MitchellLeeKane <mkane@dal.ca>
2018-02-06 11:10:51 -0400

    overcommited, removed extra tests

--- app/src/test/java/com/example/mitchellkane/assignment2/ValidatorTest.java
+++ app/src/test/java/com/example/mitchellkane/assignment2/ValidatorTest.java
@@ -34,53 +34,6 @@ public class ValidatorTest {
         assertTrue(v.greater7("itsEight"));
         assertTrue(v.greater7("pineapple"));
     }
-
-    @Test // test for upper and lower case
-    public void checkCap() throws Exception {
-        assertFalse(v.hasCap(""));
-        assertFalse(v.hasCap("a"));
-        assertFalse(v.hasCap("A"));
-        assertTrue(v.hasCap("Password"));
-        assertTrue(v.hasCap("PaSsWoRd"));
-        assertFalse(v.hasCap("password"));
-        assertFalse(v.hasCap("PASSWORD"));
-    }
-
-    @Test // test for contains a number
-    public void checkNum() throws Exception {
-        assertFalse(v.hasNum(""));
-        assertFalse(v.hasNum("a"));
-        assertFalse(v.hasNum("A"));
-        assertTrue(v.hasNum("1"));
-        assertTrue(v.hasNum("Password1"));
-        assertFalse(v.hasNum("PASSWORD"));
-    }
-
-    @Test // test for contains a special character
-    public void checkSpecial() throws Exception {
-        assertFalse(v.hasSpecial(""));
-        assertFalse(v.hasSpecial("a"));
-        assertFalse(v.hasSpecial("A"));
-        assertFalse(v.hasSpecial("1"));
-        assertTrue(v.hasSpecial(";"));
-        assertTrue(v.hasSpecial("Password!"));
-        assertTrue(v.hasSpecial("1@"));
-        assertTrue(v.hasSpecial("#####"));
-        assertTrue(v.hasSpecial("$41t"));
-    }
-
-    /* test  of my validate function that returns
-    number of tests passed */
-    @Test
-    public void numPassed() throws Exception {
-        assertEquals(1, v.validate("password"));
-        assertEquals(2, v.validate("Password"));
-        assertEquals(5, v.validate("Password1!"));
-        assertEquals(4, v.validate("!2Ac"));
-        assertEquals(1, v.validate(""));
-        assertEquals(1, v.validate("a"));
-        assertEquals(3, v.validate("B4n4n4"));
-        assertEquals(5, v.validate("!2aA-----"));
-    }
+    
 
 }
